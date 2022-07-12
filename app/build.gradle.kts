f3918f238ca01c1ca876d4684a6fa4f12ca80f5c
AntoinePurnelle <antoine.purnelle@gmail.com>
2022-07-12 14:10:19 +0200

    Version bumped to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "net.ouftech.oufmime"
         minSdk = 26
         targetSdk = 31
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
